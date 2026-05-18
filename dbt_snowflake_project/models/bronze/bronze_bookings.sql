0b87322482503affe76658827ddb74cb73557ab5
Adarsh Arora <adarsh.arora@zs.com>
2026-05-18 17:51:54 +0000

    bronze layer in-progress

---
+++ dbt_snowflake_project/models/bronze/bronze_bookings.sql
@@ -0,0 +1,11 @@
+--select * from {{ source('staging','bookings') }}
+
+{# {% set incremental_flag = 1 %}
+{% set incremental_column = 'created_at' %} #}
+
+{{  config(materialized='incremental') }}
+
+select * from {{ source('staging','bookings') }}
+{% if is_incremental() %}
+    WHERE created_at> (SELECT COALESCE(MAX(created_at),'1900-01-01') from {{ this }})
+{% endif %}
